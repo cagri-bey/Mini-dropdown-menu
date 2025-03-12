2af1c342f46c7e1aa8bc328820e2e2544f448d6f
Çağrı Aydın <cagriaydinoffical@gmail.com>
2025-03-13 02:51:23 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+let profile = document.querySelector(".profile");
+let menu = document.querySelector(".menu");
+
+
+profile.addEventListener("click", () => {
+    menu.classList.toggle("active")
+})
+
+
